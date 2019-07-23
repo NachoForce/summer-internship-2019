7fa5fef012b413fa6b85c95b141dbd83d2a7ed94
Akshay Bablani <aks@Akshays-MacBook-Pro.local>
2019-07-23 11:14:29 -0700

    Added Scripts 1-5

---
+++ beginning-scripting-1-5/Assets/VariablesAndFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    int myInt = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInt = CountToTen(myInt);
+        Debug.Log(myInt);
+    }
+
+    int CountToTen(int number) {
+        int ret = number; 
+        while (ret != 10) {
+            Debug.Log("Your number is " + ret);
+            ret++;
+            if (ret == 10) {
+                Debug.Log("Your number is 10!");
+            }
+        }
+        return ret;
+    }
+    // Update is called once per frame
+}
