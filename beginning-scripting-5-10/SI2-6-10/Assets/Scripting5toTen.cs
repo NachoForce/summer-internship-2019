9a6234680c8df3365071278d0d0c089094a77c16
NachoForce <53190731+NachoForce@users.noreply.github.com>
2019-07-23 15:29:50 -0700

    Si2 6 5 10 (#6)
    * Deleted Temp files
    
    * Deleted Log Files
    
    * Deleted Library
    
    * Updated light on Cube

---
+++ beginning-scripting-5-10/SI2-6-10/Assets/Scripting5toTen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripting5toTen : MonoBehaviour
+{
+    public float speed = 5.0f; 
+    Rigidbody rb;
+    public Light myLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // myLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            rb.velocity = transform.right * speed;
+            myLight.enabled = true;
+        } 
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            rb.velocity = -transform.right * speed;
+            myLight.enabled = false; 
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            rb.velocity = new Vector3(0,0,1) * speed;
+        }
+    }
+}
