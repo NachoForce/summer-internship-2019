e9cb2dd8c023b4ce6ee7a30afea5083a65b8c79e
Akshay Bablani <aks@Akshays-MacBook-Pro.local>
2019-07-25 15:04:54 -0700

    Added a gif to README

---
+++ beginning-scripting-10-15/SI2-7-10-15/Assets/CollisionDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDestroy : MonoBehaviour
+{
+
+    void onCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hi");
+        if (collision.gameObject.name == "Bullet") {
+
+            Destroy(collision.gameObject);
+        }
+    }
+}
